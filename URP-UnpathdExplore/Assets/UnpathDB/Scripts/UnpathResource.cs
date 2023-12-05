8ef91af7960704730d651c301b7701b78ce27e57
JFlint-Unpathd <johnflint1750@gmail.com>
2023-12-05 14:14:05 +0000

    reset scene
    -all working, new working scene name is interaction

--- URP-UnpathdExplore/Assets/UnpathDB/Scripts/UnpathResource.cs
+++ URP-UnpathdExplore/Assets/UnpathDB/Scripts/UnpathResource.cs
@@ -5,7 +5,8 @@ using TMPro;
 
 public class UnpathResource : MonoBehaviour {
 
-    public string m_Name;
+    //public string m_Name;
+    public string m_Label;
     public string m_Title;
     public string m_Description;
     public LatLng m_LatLng;
