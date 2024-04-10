e8c1576ce6ad49013197f342833e6bd4d23f7303
JFlint-Unpathd <145466246+JFlint-Unpathd@users.noreply.github.com>
2024-04-10 10:06:41 +0100

    cleaned newest asset added
    - deleted a bunch of of scenes

--- URP-UnpathdExplore/Assets/Fog/URPCookbookAssets/Scenes/Instancing/Scripts/Common/AbstractGrassDrawer.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace GPUInstancedGrass.Common {
-	public abstract class AbstractGrassDrawer : MonoBehaviour {
-		public abstract void Init(Vector2[,] grassEntities, Vector2 fieldSize);
-
-		public abstract void UpdatePositions(Vector2Int bottomLeftCameraCell, Vector2Int topRightCameraCell);
-	}
-}
