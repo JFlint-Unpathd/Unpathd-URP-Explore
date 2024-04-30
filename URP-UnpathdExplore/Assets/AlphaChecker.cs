b955eb0153e9f5de065c3497abd450eaadc05f68
JFlint-Unpathd <johnflint1750@gmail.com>
2024-04-30 14:45:56 +0100

    Wrote shader from tut, works but not highly desired
    https://www.youtube.com/watch?v=sXevaQ8cM2c

---
+++ URP-UnpathdExplore/Assets/AlphaChecker.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEditor;
+
+public class AlphaChecker : MonoBehaviour
+{
+    [SerializeField] public Texture2D texture;
+
+    void Start()
+    {
+        var textureImporter = AssetImporter.GetAtPath(AssetDatabase.GetAssetPath(texture)) as TextureImporter;
+
+        if (textureImporter != null)
+        {
+            bool hasAlpha = textureImporter.DoesSourceTextureHaveAlpha();
+
+            Debug.Log("Does the texture have an alpha channel? " + hasAlpha);
+        }
+    }
+}
