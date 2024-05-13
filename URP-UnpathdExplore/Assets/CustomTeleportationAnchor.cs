94fdfd1f9bf97a75fd26b5db73540b189b9e05a8
JFlint-Unpathd <145466246+JFlint-Unpathd@users.noreply.github.com>
2024-05-13 15:48:32 +0100

    no update
    - custom anchor script
    - content associated on custom anchor script, both modalities work now
    - ui and teleport

---
+++ URP-UnpathdExplore/Assets/CustomTeleportationAnchor.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class CustomTeleportationAnchor : TeleportationAnchor
+{
+    public GameObject associatedContent;
+
+    protected override bool GenerateTeleportRequest(IXRInteractor interactor, RaycastHit raycastHit, ref TeleportRequest teleportRequest)
+    {
+        if (teleportAnchorTransform == null)
+            return false;
+
+        teleportRequest.destinationPosition = teleportAnchorTransform.position;
+        teleportRequest.destinationRotation = teleportAnchorTransform.rotation;
+
+        // Disable all other content
+        foreach (CustomTeleportationAnchor anchor in FindObjectsOfType<CustomTeleportationAnchor>())
+        {
+            if (anchor != this && anchor.associatedContent != null)
+            {
+                anchor.associatedContent.SetActive(false);
+            }
+        }
+
+        // Enable the content associated with this anchor
+        if (associatedContent != null)
+        {
+            associatedContent.SetActive(true);
+        }
+
+        return true;
+    }
+}
